1002bbd80352b88c779410d2dcd079f1f61e68b5
mudzunga007 <mmatakalaa@gmail.com>
2023-06-30 05:20:54 -0700

    updated

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,5 +1,6 @@
 #include "main.h"
 
+
 /**
 * rev_string - reverse array
 * @n: integer params
@@ -7,24 +8,31 @@
 */
 
 void rev_string(char *n)
+
 {
+
 int i = 0;
+
 int j = 0;
+
 char temp;
 
 while (*(n + i) != '\0')
+
 {
 i++;
 }
-i--;
 
+i--;
 for (j = 0; j < i; j++, i--)
 {
 temp = *(n + j);
+
 *(n + j) = *(n + i);
 *(n + i) = temp;
 }
 }
+
 
 /**
 * infinite_add - add 2 numbers together
@@ -35,9 +43,13 @@ temp = *(n + j);
 * Return: pointer to calling function
 */
 
+
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
+
 {
+
 int overflow = 0, i = 0, j = 0, digits = 0;
+
 int val1 = 0, val2 = 0, temp_tot = 0;
 
 while (*(n1 + i) != '\0')
