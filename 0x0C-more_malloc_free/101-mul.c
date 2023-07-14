d98c3aeb1eb032b1d4d20d191ea23b04bcc7fc37
mudzunga007 <mmatakalaa@gmail.com>
2023-07-14 09:47:16 -0700

    update

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,7 +1,5 @@
 #include "main.h"
-
 #include <stdlib.h>
-
 #include <stdio.h>
 
 #define ERR_MSG "Error"
@@ -9,13 +7,11 @@
 /**
 * is_digit - checks if a string contains a non-digit char
 * @s: string to be evaluated
+*
 * Return: 0 if a non-digit is found, 1 otherwise
 */
-
 int is_digit(char *s)
-
 {
-
 int i = 0;
 
 while (s[i])
@@ -30,12 +26,11 @@ return (1);
 /**
 * _strlen - returns the length of a string
 * @s: string to evaluate
+*
 * Return: the length of the string
 */
-
 int _strlen(char *s)
 {
-
 int i = 0;
 
 while (s[i] != '\0')
@@ -48,11 +43,9 @@ return (i);
 /**
 * errors - handles errors for main
 */
-
 void errors(void)
 {
-printf(6"Error\n");
-
+printf("Error\n");
 exit(98);
 }
 
@@ -60,13 +53,12 @@ exit(98);
 * main - multiplies two positive numbers
 * @argc: number of arguments
 * @argv: array of arguments
+*
 * Return: always 0 (Success)
 */
-
 int main(int argc, char *argv[])
 {
 char *s1, *s2;
-
 int len1, len2, len, i, carry, digit1, digit2, *result, a = 0;
 
 s1 = argv[1], s2 = argv[2];
@@ -76,7 +68,6 @@ len1 = _strlen(s1);
 len2 = _strlen(s2);
 len = len1 + len2 + 1;
 result = malloc(sizeof(int) * len);
-
 if (!result)
 return (1);
 for (i = 0; i <= len1 + len2; i++)
@@ -85,7 +76,6 @@ for (len1 = len1 - 1; len1 >= 0; len1--)
 {
 digit1 = s1[len1] - '0';
 carry = 0;
-
 for (len2 = _strlen(s2) - 1; len2 >= 0; len2--)
 {
 digit2 = s2[len2] - '0';
